89a43d0062a14e8a909b404ef0ca0e7e8a5b0339
Zion Falk <zion.falk@elev.ga.ntig.se>
2023-09-06 09:53:42 +0200

    change

---
+++ Lucktest2/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("What is your name?");
+
+string name = Console.ReadLine();
+
+Console.WriteLine($"{name}, huh? What is your favourite body part?");
+
+string part = Console.ReadLine();
+
+Console.WriteLine("intresting...What about pokemon?");
+
+string pokemon = Console.ReadLine();
+
+Console.WriteLine($"WOW okay then {name} I bet you would love to see {pokemon}'s {part} huh? Disgusting. That's what you are. Mr.{name}. {pokemon} looks super young right? But you just look at their {part} no matter the age. You serve zero purpose");
+Console.ReadLine();
+
+Console.WriteLine("go away");
+
+Console.ReadLine();
